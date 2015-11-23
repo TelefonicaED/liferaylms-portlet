6da629eec29d8edddad1af4a12266604861e0259
je105585 <je10585@imartinmora.educateterra.com>
2015-11-23 12:42:22 +0100

    Desarrollo

--- docroot/WEB-INF/src/com/liferay/lms/service/impl/LearningActivityTryLocalServiceImpl.java
+++ docroot/WEB-INF/src/com/liferay/lms/service/impl/LearningActivityTryLocalServiceImpl.java
@@ -18,9 +18,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.liferay.lms.NoSuchLearningActivityException;
 import com.liferay.lms.auditing.AuditConstants;
 import com.liferay.lms.auditing.AuditingLogFactory;
+import com.liferay.lms.cassandra.ExtConexionCassandra;
 import com.liferay.lms.model.LearningActivity;
 import com.liferay.lms.model.LearningActivityResult;
 import com.liferay.lms.model.LearningActivityTry;
@@ -30,12 +36,15 @@ import com.liferay.lms.service.LearningActivityLocalServiceUtil;
 import com.liferay.lms.service.LearningActivityResultLocalServiceUtil;
 import com.liferay.lms.service.ModuleResultLocalServiceUtil;
 import com.liferay.lms.service.base.LearningActivityTryLocalServiceBaseImpl;
+import com.liferay.lms.service.cassandrapersistence.LearningActivityTryPersistenceCassandra;
 import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.DocumentException;
@@ -68,9 +77,22 @@ import com.liferay.portal.service.UserLocalServiceUtil;
  * @see com.liferay.lms.service.base.LearningActivityTryLocalServiceBaseImpl
  * @see com.liferay.lms.service.LearningActivityTryLocalServiceUtil
  */
-public class LearningActivityTryLocalServiceImpl
-	extends LearningActivityTryLocalServiceBaseImpl {
-	@Override
+public class LearningActivityTryLocalServiceImpl extends LearningActivityTryLocalServiceBaseImpl {
+	
+	
+	public com.liferay.lms.service.persistence.LearningActivityTryPersistence persistence=null;
+	
+	
+	
+	public com.liferay.lms.service.persistence.LearningActivityTryPersistence getPersistence(){
+		if(PropsUtil.get("persistencemethod").equals(String.valueOf("Cassandra")))
+			return new LearningActivityTryPersistenceCassandra();
+		else
+			return learningActivityTryPersistence;
+	}
+	
+	
+	
 	public LearningActivityTry updateLearningActivityTry(
 			LearningActivityTry learningActivityTry) throws SystemException {
 		
@@ -78,15 +100,17 @@ public class LearningActivityTryLocalServiceImpl
 	}
 	public long getLearningActivityTryByActUserCount(long actId,long userId) throws SystemException
 	{
-		return learningActivityTryPersistence.countByact_u(actId, userId);
+		return getPersistence().countByact_u(actId, userId);
 	}
 	public void deleteUserTries(long actId,long userId) throws SystemException, PortalException
 	{
-		java.util.List<LearningActivityTry> userTries=learningActivityTryPersistence.findByact_u(actId, userId);
+		java.util.List<LearningActivityTry> userTries= getPersistence().findByact_u(actId, userId);
 		LearningActivity larn=LearningActivityLocalServiceUtil.getLearningActivity(actId);
 		for(LearningActivityTry userTry:userTries)
 		{
-			this.deleteLearningActivityTry(userTry.getLatId());
+			
+			//this.deleteLearningActivityTry(userTry.getLatId());
+			getPersistence().remove(userTry.getLatId());
 			
 			//auditing
 			ServiceContext serviceContext = ServiceContextThreadLocal.getServiceContext();
@@ -123,8 +147,13 @@ public class LearningActivityTryLocalServiceImpl
 	
 	public java.util.List<LearningActivityTry> getLearningActivityTryByActUser(long actId,long userId) throws SystemException
 	{
-		return learningActivityTryPersistence.findByact_u(actId, userId);
+		return getPersistence().findByact_u(actId, userId);
 	}
+	
+	
+	
+	
+	
 	@Override
 	public LearningActivityTry updateLearningActivityTry(
 			LearningActivityTry learningActivityTry, boolean merge)
@@ -135,13 +164,23 @@ public class LearningActivityTryLocalServiceImpl
 		{
 			LearningActivityResultLocalServiceUtil.update(learningActivityTry)	;
 		}
-		return super.updateLearningActivityTry(learningActivityTry, merge);
+
+		//return super.updateLearningActivityTry(learningActivityTry, merge);
+		
+		learningActivityTry.setNew(false);
+		return  getPersistence().update(learningActivityTry, merge);
+		
 	}
 		catch(PortalException e)
 		{
 			throw new SystemException(e);
 		}
 	}
+	
+
+	
+	
+	
 
 	public LearningActivityTry createLearningActivityTry(long actId,ServiceContext serviceContext) throws SystemException, PortalException
 	{
@@ -152,12 +191,14 @@ public class LearningActivityTryLocalServiceImpl
 		}
 		
 		LearningActivityTry larnt =
-			learningActivityTryPersistence.create(counterLocalService.increment(
+				 getPersistence().create(counterLocalService.increment(
 					LearningActivityTry.class.getName()));
 		larnt.setUserId(serviceContext.getUserId());
 		larnt.setActId(actId);
 		larnt.setStartDate(serviceContext.getCreateDate(null));
-		learningActivityTryPersistence.update(larnt, true);
+		
+		
+		getPersistence().update(larnt, true);
 		learningActivityResultLocalService.update(larnt);
 		
 		courseResultLocalService.softInitializeByGroupIdAndUserId(learningActivity.getGroupId(), serviceContext.getUserId());
@@ -171,40 +212,67 @@ public class LearningActivityTryLocalServiceImpl
 	
 	@SuppressWarnings("unchecked")
 	public List<User> getUsersByLearningActivity(long actId) throws SystemException, PortalException
-	{ 			
-		List<User> users = new ArrayList<User>();
+	{ 
 		
-		ClassLoader classLoader = (ClassLoader) PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(), "portletClassLoader");
-		DynamicQuery consulta = DynamicQueryFactoryUtil.forClass(LearningActivityTry.class, classLoader)
-					.add(PropertyFactoryUtil.forName("actId").eq(new Long(actId)));
-					
-		List<LearningActivityTry> activities = (List<LearningActivityTry>)learningActivityTryPersistence.findWithDynamicQuery(consulta);
-
-		for(LearningActivityTry activity:activities){
-			Long uId = activity.getUserId();
-			User u = UserLocalServiceUtil.getUserById(uId.longValue());
-			if(u!=null && !users.contains(u))
-				users.add(u);
+		List<User> users = new ArrayList<User>();
+		//Cassandra
+		if(PropsUtil.get("persistencemethod").equals(String.valueOf("Cassandra"))){
+			////Search LearningActivityTry 
+			List<LearningActivityTry> activities = getPersistence().findByact(actId,QueryUtil.ALL_POS,QueryUtil.ALL_POS);
+			for(LearningActivityTry activity:activities){
+				Long uId = activity.getUserId();
+				User	 u = UserLocalServiceUtil.getUserById(uId.longValue());
+				if(u!=null && !users.contains(u))
+					users.add(u);
+			}
+		}else{
+			//Liferay
+			ClassLoader classLoader = (ClassLoader) PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(), "portletClassLoader");
+			DynamicQuery consulta = DynamicQueryFactoryUtil.forClass(LearningActivityTry.class, classLoader)
+						.add(PropertyFactoryUtil.forName("actId").eq(new Long(actId)));
+			List<LearningActivityTry> activities = (List<LearningActivityTry>)learningActivityTryPersistence.findWithDynamicQuery(consulta);
+			for(LearningActivityTry activity:activities){
+				Long uId = activity.getUserId();
+				User	 u = UserLocalServiceUtil.getUserById(uId.longValue());
+				if(u!=null && !users.contains(u))
+					users.add(u);
+			}
 		}
 		return users;		
+				
 	}
 	
 	@SuppressWarnings("unchecked")
 	public LearningActivityTry getLastLearningActivityTryByActivityAndUser(long actId,long userId) throws SystemException, PortalException
-	{ 			
-		DynamicQuery consulta = DynamicQueryFactoryUtil.forClass(LearningActivityTry.class, (ClassLoader)PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(),
-				"portletClassLoader"))
-					.add(PropertyFactoryUtil.forName("actId").eq(new Long(actId)))
-					.add(PropertyFactoryUtil.forName("userId").eq(new Long(userId)))
-					.addOrder(PropertyFactoryUtil.forName("endDate").desc());
-					
-		List<LearningActivityTry> activities = (List<LearningActivityTry>)learningActivityTryPersistence.findWithDynamicQuery(consulta);
+	{ 	
+	
+		
+		//Cassandra
+		if(PropsUtil.get("persistencemethod").equals(String.valueOf("Cassandra"))){
+			
+			
+			
+			
+			
+		}else{
+			DynamicQuery consulta = DynamicQueryFactoryUtil.forClass(LearningActivityTry.class, (ClassLoader)PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(),
+					"portletClassLoader"))
+						.add(PropertyFactoryUtil.forName("actId").eq(new Long(actId)))
+						.add(PropertyFactoryUtil.forName("userId").eq(new Long(userId)))
+						.addOrder(PropertyFactoryUtil.forName("endDate").desc());
+						
+			List<LearningActivityTry> activities = (List<LearningActivityTry>)learningActivityTryPersistence.findWithDynamicQuery(consulta);
 
-		for(LearningActivityTry activity:activities){
-			//Necesitamos la primera, que est� ordenada por la �ltima realizada.
-			return activity;
+			for(LearningActivityTry activity:activities){
+				//Necesitamos la primera, que est� ordenada por la �ltima realizada.
+				return activity;
+			}
+			
 		}
-		return null;		
+		
+
+		return null;
+			
 	}
 	@SuppressWarnings("unchecked")	
 	public LearningActivityTry createOrDuplicateLast(long actId,ServiceContext serviceContext) throws SystemException, PortalException
@@ -256,13 +324,16 @@ public class LearningActivityTryLocalServiceImpl
 	}
 	
 	public int getTriesCountByActivityAndUser(long actId,long userId) throws SystemException, PortalException
-	{ 			
-		return learningActivityTryPersistence.countByact_u(actId, userId);	
+	{ 		
+		return getPersistence().countByact_u(actId, userId);
+		//return learningActivityTryPersistence.countByact_u(actId, userId);
+	
 	}
 	
 	public int getTriesCountByActivity(long actId) throws SystemException, PortalException
 	{ 			
-		return learningActivityTryPersistence.countByact(actId);	
+		return getPersistence().countByact(actId);	
+		//return learningActivityTryPersistence.countByact(actId);	
 	}
 	
 	public HashMap<Long, Long> getMapTryResultData(long actId,long userId) throws SystemException, PortalException 
