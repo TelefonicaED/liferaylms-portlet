317b83817413c17eab0bc4c6d7893f35e480164e
je105585 <je10585@imartinmora.educateterra.com>
2016-01-20 12:21:23 +0100

    desarrollo

--- docroot/WEB-INF/src/com/liferay/lms/cassandra/ExtConexionCassandra.java
+++ docroot/WEB-INF/src/com/liferay/lms/cassandra/ExtConexionCassandra.java
@@ -29,6 +29,32 @@ public class ExtConexionCassandra  extends MVCPortlet{
 	public static PreparedStatement getLastLearningActivityTryByActivityAndUser;
 	public static PreparedStatement findByactByUserByEnDate_uStatement;
 	public static PreparedStatement updateLearningActivityTry_Statement;
+	
+	
+	
+	//LeareningActivityResult
+	
+	public static PreparedStatement countByact_user;
+	public static PreparedStatement fetchByact_user;
+	public static PreparedStatement updateLearningActivityResult_Statement;
+	public static PreparedStatement searchBylar_Statement;
+	public static PreparedStatement countByap;
+	public static PreparedStatement countByuser;
+	public static PreparedStatement countAll;
+	public static PreparedStatement countByac;
+	public static PreparedStatement countByap_endDate;
+	public static PreparedStatement countByUuid;
+	
+	
+
+	public static PreparedStatement searchByActId_userId_Statement;
+	public static PreparedStatement searchByActId_userId_passedStatement;
+	public static PreparedStatement searchByActId_NotpassedStatement;
+	public static PreparedStatement avgResult_Statement;
+	public static PreparedStatement getLastEndDateByUserId_Statement;
+	public static PreparedStatement getByActId_Statement;
+	public static PreparedStatement remove_Statement;
+	
 
 	
 	
@@ -63,7 +89,7 @@ public class ExtConexionCassandra  extends MVCPortlet{
 			                  "userId bigint," + 
 			                  "startDate timestamp," +		                  
 			                  "result bigint," +
-			                  "endDate varchar," +
+			                  "endDate timestamp," +
 			                  "tryData varchar," +
 			                  "tryResultData varchar," +					                  
 			                  "comments varchar," +					                  
@@ -71,25 +97,44 @@ public class ExtConexionCassandra  extends MVCPortlet{
 			                  ");");
 		      
 		      session.execute(
+			            "CREATE TABLE IF NOT EXISTS liferay.lms_learningactivityResult (" +
+			                  "uuid_ varchar," + 
+			                  "larId bigint," +
+			                  "actId bigint," +		                  
+			                  "userId bigint," +
+			                  "result bigint," +			                  
+			                  "startDate timestamp," +		                  
+			                  "endDate timestamp," +
+			                  "latId bigint," +					                  
+			                  "comments varchar," +					                  
+			                  "passed boolean," +			                  
+			                  "PRIMARY KEY (larId)" + 
+	                          ")"); 
+	      
+		      session.execute(
 			            "CREATE INDEX IF NOT EXISTS sagi14 on liferay.lms_learningactivitytry (userId);");
 		      session.execute(
-			            "CREATE INDEX IF NOT EXISTS sagi15 on liferay.lms_learningactivitytry (endDate);");		      
+			            "CREATE INDEX IF NOT EXISTS sagi15 on liferay.lms_learningactivitytry (endDate);");	
+		      session.execute(
+			            "CREATE INDEX IF NOT EXISTS sagi16 on liferay.lms_learningactivityresult (endDate);");
+			            			      
 		      
 
     //LEARNINGACTIVITYTRY
 		      countByact_uStatement = session.prepare(
 		    		  "select count(*) from liferay.lms_learningactivitytry WHERE actId = ? AND userId = ?  ALLOW FILTERING;");		      
 		      countByact_Statement = session.prepare(
-		    		  "select count(*) from liferay.lms_learningactivitytry WHERE actId = ?; ");		
+		    		  "select count(*) from liferay.lms_learningactivitytry WHERE actId = ?  ALLOW FILTERING;; ");		
 		      findByact_uStatement= session.prepare(
 		    		  "select * from liferay.lms_learningactivitytry WHERE actId = ? AND userId = ?  ALLOW FILTERING;");
 		      remove_uStatement= session.prepare(
-		    		  "delete  from liferay.lms_learningactivitytry where latId IN ?;;");
+		    		  "delete  from liferay.lms_learningactivitytry where latId =?;");
 		      searchBylat_Statement =  session.prepare(
-		    		  "select *  from liferay.lms_learningactivitytry where latId IN ?;");
+		    		  "select *  from liferay.lms_learningactivitytry where latId= ?;");
+
 		      
 		      searchByact_Statement=  session.prepare(
-		    		  "select *  from liferay.lms_learningactivitytry where actId= ?;");
+		    		  "select *  from liferay.lms_learningactivitytry where actId= ?  ALLOW FILTERING;");	  
 		      
 		      getLastLearningActivityTryByActivityAndUser = session.prepare(
 		    		  "select *  from liferay.lms_learningactivitytry where latid=? and userId=? and endDate=? ALLOW FILTERING;");
@@ -101,20 +146,102 @@ public class ExtConexionCassandra  extends MVCPortlet{
 		      
 		      updateLearningActivityTry_Statement = session.prepare(
 		    		  " INSERT INTO  liferay.lms_learningactivitytry (latid, actid, comments, enddate, result, startdate, trydata, tryresultdata, userid, uuid_) " 
-		    			 + " VALUES (?,?,?,?,?,?,?,?,?,?);");	
+		    			 + " VALUES (?,?,?,?,?,?,?,?,?,?);");
 		      
 
+		      
+
+	 //LEARNINGACTIVITYRESULT
+		      countByact_user = session.prepare(
+		    		  "select count(*) from liferay.lms_learningactivityResult WHERE actId = ? AND userId = ?  ALLOW FILTERING;");	
+		      
+		      fetchByact_user = session.prepare(
+		    		  "select * from liferay.lms_learningactivityResult WHERE actId = ? AND userId = ?  ALLOW FILTERING;");
+		      
+		      updateLearningActivityResult_Statement= session.prepare(
+		    		  " INSERT INTO  liferay.lms_learningactivityResult (uuid_, larId, actid,userid,result,startdate,enddate,latid, comments, passed) " 
+		    			 + " VALUES (?,?,?,?,?,?,?,?,?,?);");
+		      searchBylar_Statement =  session.prepare(
+		    		  "select *  from liferay.lms_learningactivityResult where larId= ?;");
+		      
+		      countByap =  session.prepare(
+		    		  "select count(*)  from liferay.lms_learningactivityResult where actId= ?  AND passed = ?  ALLOW FILTERING;");
+		      countByap_endDate =  session.prepare(
+		    		  "select count(*)  from liferay.lms_learningactivityResult where actId= ?  AND passed = ?  and endDate = ? ALLOW FILTERING;");
+
+		      
+		      searchByActId_userId_Statement = session.prepare(
+		    		  "select *  from liferay.lms_learningactivityResult where actId= ? and userId = ?  ALLOW FILTERING;");
+		      
+	      
+		      
+		      searchByActId_userId_passedStatement = session.prepare(
+		    		  "select *  from liferay.lms_learningactivityResult where actId= ? and userId = ? and passed = ?  ALLOW FILTERING;");
+		      
+		      searchByActId_NotpassedStatement = session.prepare(
+		    		  "select *  from liferay.lms_learningactivityResult where actId= ?  and passed = false ALLOW FILTERING;");		
+		      
+		      avgResult_Statement =  session.prepare(
+		    		  "select avg(result)  from liferay.lms_learningactivityResult where actId= ?  ALLOW FILTERING;");		
+		      
+		      
+		      getLastEndDateByUserId_Statement  = session.prepare(
+		    		  "select *  from liferay.lms_learningactivityResult where userId= ?  ALLOW FILTERING;");		
+		      
+		      getByActId_Statement = session.prepare(
+		    		  "select *  from liferay.lms_learningactivityResult where actId= ?  ALLOW FILTERING;");	 
+		      
+		      countByUuid = session.prepare(
+		    		  "select *  from liferay.lms_learningactivityResult where uuid_= ?  ALLOW FILTERING;");	
+		      
+		      countAll =  session.prepare(
+		    		  "select count(*)  from liferay.lms_learningactivityresult;");
+		      countByuser =  session.prepare(
+		    		  "select count(*)  from liferay.lms_learningactivityresult where userId=?  ALLOW FILTERING;");
+		      countByac =   session.prepare(
+		    		  "select count(*)  from liferay.lms_learningactivityresult where actId=?  ALLOW FILTERING;");		 
+		      
+		      
+		      remove_Statement = session.prepare(
+				      "delete  from liferay.lms_learningactivityResult where larid IN ?;");
+		      
 		      
 		   }	
 	
 
 	   public static void connect(){
+
+           // Create session to hosts
+		   /*		   
+		   if(cluster==null){	
+			   
+			   
+           Cluster cluster = new Cluster.Builder().addContactPoints(String.valueOf("127.0.0.1")).build();
+           Metadata metadata = cluster.getMetadata();
+
+           Session session = cluster.connect();
+
+           Metadata metadata = cluster.getMetadata();
+           System.out.println(String.format("Connected to cluster '%s' on %s.", metadata.getClusterName(), metadata.getAllHosts()));
+		   }
+*/
+		   
+		   
 		  if(cluster==null){
-		      cluster = Cluster.builder()
-		            .addContactPoint(node)
-		            .build();
+			  
+			  
+			  
+			  
+		      cluster = Cluster.builder().addContactPoint(node).build();
+  	         
+
 		      Metadata metadata = cluster.getMetadata();
-		      System.out.printf("Connected to cluster: %s\n", 
+		     
+  	         
+  	         
+  	         
+  	         
+  	         System.out.printf("Connected to cluster: %s\n", 
 		            metadata.getClusterName());
 		      for ( Host host : metadata.getAllHosts() ) {
 		         System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
@@ -123,6 +250,7 @@ public class ExtConexionCassandra  extends MVCPortlet{
 		      
 		      session = cluster.connect();
 		  }
+	  
 		   
 	  }
 
