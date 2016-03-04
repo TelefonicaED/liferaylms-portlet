1feb20de6de7e8f3b54efa7ae57f41bb1d8964d5
je10413 <je10413@MPereira.educaterra.com>
2016-03-04 08:20:07 +0100

    Cambios para las efectos drag and drop en mobile

--- docroot/js/test.js
+++ docroot/js/test.js
@@ -1,12 +1,17 @@
-AUI().ready('node','aui-io-request','aui-parse-content','aui-sortable','dd-constrain', 'dd-proxy', 'dd-drop',function(A) {
-
-	A.all('div.question-page-current ul.sortable').each(function(node) {
-		new A.Sortable(
-		{
-			container: node,
-		    nodes: 'li'
+$(function() {
+	var ismobile=navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
+	if(ismobile){
+		doStuff();
+	}else{
+		AUI().ready('node','aui-io-request','aui-parse-content','aui-sortable','dd-constrain', 'dd-proxy', 'dd-drop',function(A) {
+		A.all('div.question-page-current ul.sortable').each(function(node) {
+			new A.Sortable(
+			{
+			   container: node,
+			    nodes: 'li'
+			});
 		});
-	});
+
 	
     A.DD.DDM.on('drag:start', function(e) {
         var drag = e.target;
@@ -28,7 +33,6 @@ AUI().ready('node','aui-io-request','aui-parse-content','aui-sortable','dd-const
     	
 	A.all('.draganddrop').each(function(node) {
 		var elem = '#'+node.attr('id');
-		
 		A.all(elem + ' > .items > div').each(function(v,k) {
 			var dd = new A.DD.Drag({
 				container: elem,
@@ -99,3 +103,113 @@ AUI().ready('node','aui-io-request','aui-parse-content','aui-sortable','dd-const
 	});
 	
 });
+
+
+}
+
+});
+
+function doStuff() {
+
+	$.each($(".items>div"), function(index, value) {
+		$(value).draggable({
+			revert : "invalid",
+			tolerance : "fit",
+			start : function(e, ui) {
+				// console.log("DRAG START");
+				var drag = e.target;
+				//console.log(drag);
+			}
+
+		});
+
+	});
+
+	$.each($(".drop div"), function(index, value) {
+		if ($(value).hasClass('occupied')) {
+			$(value).draggable({
+				revert : "invalid",
+				tolerance : "fit",
+				start : function(e, ui) {
+					//console.log("DRAG FROM RIGHT TO LEFT");
+					// var drag = e.target;
+					// console.log(drag);
+				}
+
+			});
+
+		}
+
+	});
+
+	// let the gallery be droppable as well, accepting items from the trash
+	$(".items")
+			.droppable(
+					{
+						accept : ".drop div",
+
+						drop : function(event, ui) {
+							
+							var drop = event.target;
+							
+							drag = ui.draggable[0];
+							var padre = $(drag).parent();
+							
+							$(drag).css("left", "0")
+							$(drag).css("top", "0")
+							
+							if ($(drag).prop('tagName').toLowerCase() === 'div'
+									&& $(padre).hasClass('drop-containers')) {
+								$(drop).append(drag);
+								var idPadre = $(padre).attr('id');
+								var textoPadre = idPadre.replace(/[^\d]/g, '');
+								$(padre).text(
+										Liferay.Language
+												.get('drop', textoPadre));
+								$(padre).removeClass('occupied');
+								$(padre).addClass('base');
+								$(
+										'input[name="' + $(padre).attr("name")
+												+ 'hidden"]').val(-1);
+							}
+
+						}
+
+					});
+
+	$(".drop-containers").droppable(
+			{
+				drop : function(event, ui) {
+					
+
+					var drop = event.target;
+					
+					drag = ui.draggable[0];
+					var padre = $(drag).parent();
+					
+					$(drag).css("left", "0")
+					$(drag).css("top", "0")
+					
+					if (!$(drop).hasClass('occupied')
+							&& $(drag).prop('tagName').toLowerCase() === 'div'
+							&& $(padre).hasClass('items')) {
+						$(drop).text('');
+						$(drop).append(drag);
+						$(drop).addClass('occupied');
+						$(drop).removeClass('base');
+						$('input[name="' + $(drop).attr("name") + 'hidden"]')
+								.val($(drag).attr('id'));
+					}
+
+				}
+			});
+
+	$("div.question-page-current ul.sortable").sortable({
+
+		start : function(e, ui) {
+			var drag = e.target;
+		}
+
+	});
+
+}
