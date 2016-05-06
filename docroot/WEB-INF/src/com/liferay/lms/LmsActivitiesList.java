261fdb45a6b2bd2ed061b9fe58db54e8109ddc2c
Jorge Rodriguez Fernandez <jorge.rodriguezfernandez.ext@telefonica.com>
2016-05-06 10:57:18 +0200

    Quitadas trazas no usadas de log y System.out.printlns

--- docroot/WEB-INF/src/com/liferay/lms/LmsActivitiesList.java
+++ docroot/WEB-INF/src/com/liferay/lms/LmsActivitiesList.java
@@ -617,7 +617,6 @@ public class LmsActivitiesList extends MVCPortlet {
 				
 				if(precedences!=null && precedences.size()>0){
 					for(LearningActivity precedence : precedences){
-						log.error("PRECEDENCE SET TO 0:  "+precedence.getActId());
 						precedence.setPrecedence(0);
 						LearningActivityLocalServiceUtil.updateLearningActivity(precedence);
 					}
