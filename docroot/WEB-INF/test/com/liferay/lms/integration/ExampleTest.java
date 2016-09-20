ef5f6983fdfb1b9529689182d7fe656727836bd0
Carlos Vicente <vicencarlos@gmail.com>
2016-09-20 13:24:39 +0200

    adding tests

---
+++ docroot/WEB-INF/test/com/liferay/lms/integration/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.liferay.lms.integration;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExampleTest {
+	@Test
+	public void testDomain1() throws Exception
+	{
+		Assert.assertTrue("mensajito", true);
+	}
+
+}
