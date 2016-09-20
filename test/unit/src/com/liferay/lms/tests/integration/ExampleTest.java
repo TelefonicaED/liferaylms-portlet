21406d53ba0c8dc94b4ea7da222851712ba1a8ff
Carlos Vicente <vicencarlos@gmail.com>
2016-09-20 17:55:51 +0200

    testing testing

---
+++ test/unit/src/com/liferay/lms/tests/integration/ExampleTest.java
@@ -0,0 +1,15 @@
+package com.liferay.lms.integration;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.liferay.portal.kernel.test.TestCase;
+
+public class ExampleTest extends TestCase {
+	@Test
+	public void testDomain1() throws Exception
+	{
+		Assert.assertTrue("mensajito", false);
+	}
+
+}
