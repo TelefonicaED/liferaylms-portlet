b069adcc95687e38fd2e41134cf37dd3c414dcec
je10436 <virginia.martinagudo@telefonica.com>
2016-12-12 17:47:30 +0100

    Quitado try y catch de todo en las asignaciones p2p.

--- docroot/WEB-INF/src/com/liferay/lms/P2PAssignations.java
+++ docroot/WEB-INF/src/com/liferay/lms/P2PAssignations.java
@@ -31,97 +31,135 @@ public class P2PAssignations implements MessageListener {
 	/* DEPRECATED */
 	public void asignP2PActivities(){
 
+		//Obtener el dia de ayer
+		Calendar yesterday = Calendar.getInstance();
+		yesterday.set(Calendar.DAY_OF_YEAR, yesterday.get(Calendar.DAY_OF_YEAR)-1);
+		
+		//Obtener la lista de actividades p2p de usuarios que entregaron la actividad en el dia.
+		List<P2pActivity> activities = null;
 		try {
-			//Obtener el dia de ayer
-			Calendar yesterday = Calendar.getInstance();
-			yesterday.set(Calendar.DAY_OF_YEAR, yesterday.get(Calendar.DAY_OF_YEAR)-1);
-			
-			//Obtener la lista de actividades p2p de usuarios que entregaron la actividad en el dia.
-			List<P2pActivity> activities = P2pActivityLocalServiceUtil.getP2PActivitiesInDay(yesterday);
-			
+			activities = P2pActivityLocalServiceUtil.getP2PActivitiesInDay(yesterday);
+		} catch (PortalException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SystemException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		if(activities != null){
 			//Recorrer la lista de usuario para asignar.
+			long actId = 0;
+			int numAsigns = 3;
+			String validations =  null;
+			List<User> users = null;
 			for(P2pActivity activity:activities){
-				long actId = activity.getActId();
+				actId = activity.getActId();
 			
 				//Obtener la lista de usuario que van a corregir la actividad
-				int numAsigns = 3;
-				String validations = LearningActivityLocalServiceUtil.getExtraContentValue(actId,"validaciones");
+				numAsigns = 3;
+				try {
+					validations = LearningActivityLocalServiceUtil.getExtraContentValue(actId,"validaciones");
+
+						numAsigns = Integer.valueOf(validations);
+				} catch (SystemException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 				
 				try {
-					numAsigns = Integer.valueOf(validations);
-				} catch (Exception e) {}
+					users = P2pActivityLocalServiceUtil.getUsersToCorrectP2P(actId, activity.getUserId(), numAsigns, yesterday);
+					
+					//Asignar al usuario que entrega sus correctores.
+					P2pActivityCorrectionsLocalServiceUtil.asignP2PCorrectionsToUsers(actId, activity.getP2pActivityId(), users);
+				} catch (PortalException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SystemException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				
-				List<User> users = P2pActivityLocalServiceUtil.getUsersToCorrectP2P(actId, activity.getUserId(), numAsigns, yesterday);
-				
-				//Asignar al usuario que entrega sus correctores.
-				P2pActivityCorrectionsLocalServiceUtil.asignP2PCorrectionsToUsers(actId, activity.getP2pActivityId(), users);
 			}
-
-		} catch (PortalException e) {
-			// TODO Auto-generated catch block
-		} catch (SystemException e) {
-			// TODO Auto-generated catch block
 		}
-	
+
 	}
 	
 	public void asignCorrectionsToP2PActivities(){
 		if(log.isDebugEnabled())log.debug("Check P2P asign corrections");
 
+		Calendar day = Calendar.getInstance();
+		//Obtener la lista de actividades p2p de todos los usuarios.
+		List<P2pActivity> activities = null;
+		
+		//Seleccionamos las actividades p2p entre ayer y antes de ayer.
+		DynamicQuery consulta = DynamicQueryFactoryUtil.forClass(P2pActivity.class)
+				.add(PropertyFactoryUtil.forName("asignationsCompleted").eq(false));
+		
 		try {
-			Calendar day = Calendar.getInstance();
-			//Obtener la lista de actividades p2p de todos los usuarios.
-			List<P2pActivity> activities = null;//P2pActivityLocalServiceUtil.getP2pActivities(0, P2pActivityLocalServiceUtil.getP2pActivitiesCount());
-			
-			//Seleccionamos las actividades p2p entre ayer y antes de ayer.
-			DynamicQuery consulta = DynamicQueryFactoryUtil.forClass(P2pActivity.class)
-					.add(PropertyFactoryUtil.forName("asignationsCompleted").eq(false));
-			
 			activities = (List<P2pActivity>)P2pActivityLocalServiceUtil.dynamicQuery(consulta);
+		} catch (SystemException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		if(activities != null){
 			
-			int numActivities = activities.size();
+			int numAsigns = 3;
+			String validations = null;
+			long actId = 0;
+			int activityAsignations = 0;
 			
 			//Recorrer todas las activities.
 			for(P2pActivity activity:activities){
-				long actId = activity.getActId();
+				 actId = activity.getActId();
 				
 				//Obtener las validaciones que tiene que tener la actividad.
-				int numAsigns = 3;
-				String validations = LearningActivityLocalServiceUtil.getExtraContentValue(actId,"validaciones");
-				
+				numAsigns = 3;
 				try {
+					validations = LearningActivityLocalServiceUtil.getExtraContentValue(actId,"validaciones");
 					numAsigns = Integer.valueOf(validations);
-				} catch (Exception e) {}
+				} catch (SystemException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 				
 				//Obtenemos las asignaciones que ya est�n realidas.
-				int activityAsignations =P2pActivityCorrectionsLocalServiceUtil.getNumCorrectionsAsignToUser(activity.getActId(),activity.getUserId());
-
-				//Si la actividad no tiene asignadas todas las tareas que tiene que correguir.
-				if( activityAsignations < numAsigns && !activity.isAsignationsCompleted()){
-
-					List<P2pActivity> activitiesToAsign = P2pActivityLocalServiceUtil.getP2pActivitiesToCorrect(actId, activity.getP2pActivityId(), numAsigns - activityAsignations);
-
-					if(log.isDebugEnabled()){
-						log.debug("P2P assign corrections to activity::"+activity.getActId()+"::"+activity.getUserId());
-						for(P2pActivity p2pactivity : activitiesToAsign){
-							log.debug("assign::"+p2pactivity.getUserId());
+				try {
+					activityAsignations =P2pActivityCorrectionsLocalServiceUtil.getNumCorrectionsAsignToUser(activity.getActId(),activity.getUserId());
+				
+					//Si la actividad no tiene asignadas todas las tareas que tiene que correguir.
+					if( activityAsignations < numAsigns && !activity.isAsignationsCompleted()){
+		
+						List<P2pActivity> activitiesToAsign = P2pActivityLocalServiceUtil.getP2pActivitiesToCorrect(actId, activity.getP2pActivityId(), numAsigns - activityAsignations);
+		
+						if(log.isDebugEnabled()){
+							log.debug("P2P assign corrections to activity::"+activity.getActId()+"::"+activity.getUserId());
+							for(P2pActivity p2pactivity : activitiesToAsign){
+								log.debug("assign::"+p2pactivity.getUserId());
+							}
 						}
+						
+						//Asignar al usuario que entrega sus correctores.
+						P2pActivityCorrectionsLocalServiceUtil.asignCorrectionsToP2PActivities(actId, activity.getP2pActivityId(),numAsigns - activityAsignations, activitiesToAsign, activity.getUserId());
+		
+					}else{
+						activity.setAsignationsCompleted(true);
+						P2pActivityLocalServiceUtil.updateP2pActivity(activity);
 					}
-					
-					//Asignar al usuario que entrega sus correctores.
-					P2pActivityCorrectionsLocalServiceUtil.asignCorrectionsToP2PActivities(actId, activity.getP2pActivityId(),numAsigns - activityAsignations, activitiesToAsign, activity.getUserId());
-
-				}else{
-					activity.setAsignationsCompleted(true);
-					P2pActivityLocalServiceUtil.updateP2pActivity(activity);
+				} catch (SystemException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (PortalException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 				
 			}
-
-		} catch (PortalException e) {
-			// TODO Auto-generated catch block
-		} catch (SystemException e) {
-			// TODO Auto-generated catch block
 		}
 	
 	}
