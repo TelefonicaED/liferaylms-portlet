b164ef94e59799fb4f3d4d905bde657a1a381ab3
Jorge Rodriguez Fernandez <jorge.rodriguezfernandez.ext@telefonica.com>
2017-01-24 11:22:55 +0100

    Cambio en la auditoria, para que no haga un select cada vez que inserte, se establece el merge a false;

--- docroot/WEB-INF/src/com/liferay/lms/service/impl/AuditEntryLocalServiceImpl.java
+++ docroot/WEB-INF/src/com/liferay/lms/service/impl/AuditEntryLocalServiceImpl.java
@@ -58,7 +58,7 @@ public class AuditEntryLocalServiceImpl extends AuditEntryLocalServiceBaseImpl {
 		auditEntry.setExtradata(extraData);
 		auditEntry.setClassPK(classPK);
 		auditEntry.setAssociationClassPK(associationClassPK);
-		auditEntryPersistence.update(auditEntry, true);
+		auditEntryPersistence.update(auditEntry, false);
 		
 	}
 
