153f38e74f11058897f1020866beb00ab974f378
Jorge Rodríguez Fernández <jorge.rodriguezfernandez.ext@telefonicaed.com>
2017-07-18 14:39:31 +0200

    Cambiadas variables a estáticas en el QuestionTypeRegistry debido a un problema de rendimiento.

--- docroot/WEB-INF/src/com/liferay/lms/learningactivity/questiontype/QuestionTypeRegistry.java
+++ docroot/WEB-INF/src/com/liferay/lms/learningactivity/questiontype/QuestionTypeRegistry.java
@@ -11,8 +11,8 @@ import com.liferay.portal.kernel.util.ClassLoaderProxy;
 import com.liferay.portal.kernel.util.PropsUtil;
 
 public class QuestionTypeRegistry {
-	Map<Long, QuestionType> types;
-	List<QuestionType> typesList;
+	static Map<Long, QuestionType> types;
+	static List<QuestionType> typesList;
 
 	public QuestionType getQuestionType(long typeId) {
 		return types.get(typeId);
@@ -23,42 +23,44 @@ public class QuestionTypeRegistry {
 	}
 
 	public QuestionTypeRegistry() throws SystemException {
-		Properties props = PropsUtil.getProperties("lms.question.type", true);
-		types = new HashMap<Long, QuestionType>();
-		typesList = new java.util.ArrayList<QuestionType>();
-		for (Object key:props.keySet()) {
-			String type=props.getProperty(key.toString());
-			try {
-				Class<?> c = Class.forName(type);
-			
-				QuestionType qt = (QuestionType)c.newInstance();
-				long typeId=qt.getTypeId();
-				types.put(typeId,qt);
-				typesList.add(qt);
-			} catch (ClassNotFoundException e) {
+		if( types==null ){
+			Properties props = PropsUtil.getProperties("lms.question.type", true);
+			types = new HashMap<Long, QuestionType>();
+			typesList = new java.util.ArrayList<QuestionType>();
+			for (Object key:props.keySet()) {
+				String type=props.getProperty(key.toString());
 				try {
-					String [] context = ((String) key).split("\\.");
-					Class c = Class.forName(type, true, PortletClassLoaderUtil.getClassLoader(context[1]));
-					ClassLoaderProxy clp = new ClassLoaderProxy(c.newInstance(), type, PortletClassLoaderUtil.getClassLoader(context[1]));
-					QuestionTypeClp qtclp = new QuestionTypeClp(clp);
-					long typeId=qtclp.getTypeId();
-					types.put(typeId,qtclp);
-					typesList.add(qtclp);
-				} catch (ClassNotFoundException e1) {
-					e1.printStackTrace();
-				} catch (InstantiationException e1) {
-					e1.printStackTrace();
-				} catch (IllegalAccessException e1) {
-					e1.printStackTrace();
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				} catch (Throwable e1) {
-					e1.printStackTrace();
+					Class<?> c = Class.forName(type);
+				
+					QuestionType qt = (QuestionType)c.newInstance();
+					long typeId=qt.getTypeId();
+					types.put(typeId,qt);
+					typesList.add(qt);
+				} catch (ClassNotFoundException e) {
+					try {
+						String [] context = ((String) key).split("\\.");
+						Class c = Class.forName(type, true, PortletClassLoaderUtil.getClassLoader(context[1]));
+						ClassLoaderProxy clp = new ClassLoaderProxy(c.newInstance(), type, PortletClassLoaderUtil.getClassLoader(context[1]));
+						QuestionTypeClp qtclp = new QuestionTypeClp(clp);
+						long typeId=qtclp.getTypeId();
+						types.put(typeId,qtclp);
+						typesList.add(qtclp);
+					} catch (ClassNotFoundException e1) {
+						e1.printStackTrace();
+					} catch (InstantiationException e1) {
+						e1.printStackTrace();
+					} catch (IllegalAccessException e1) {
+						e1.printStackTrace();
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					} catch (Throwable e1) {
+						e1.printStackTrace();
+					}
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
 				}
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
 			}
 		}
 	}
