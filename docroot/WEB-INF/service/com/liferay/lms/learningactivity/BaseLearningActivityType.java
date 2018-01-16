9c51016322f50db19a31f03d2f65b807a53bdc2b
Jorge Rodríguez Fernández <jorge.rodriguezfernandez.ext@telefonicaed.com>
2018-01-16 13:56:32 +0100

    129235: Cambio en el BaseLearningActivityType para que por defecto no se puedan borrar intentos de esas actividades

--- docroot/WEB-INF/service/com/liferay/lms/learningactivity/BaseLearningActivityType.java
+++ docroot/WEB-INF/service/com/liferay/lms/learningactivity/BaseLearningActivityType.java
@@ -66,7 +66,7 @@ public abstract class BaseLearningActivityType implements LearningActivityType, 
 
 	@Override
 	public boolean hasDeleteTries() {
-		return true;
+		return false;
 	}
 	
 	@Override
