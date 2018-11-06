1377d8b896337ca1709c5992a9fc22819134854d
ana <ana.gonzalezgalan.ext@telefonicaed.com>
2018-11-06 16:20:18 +0100

    143338: Actividad tipo test - Implementar función preview en todos los test cuando se inicia por primera vez (primer commit)

--- docroot/WEB-INF/src/com/liferay/lms/learningactivity/TestLearningActivityType.java
+++ docroot/WEB-INF/src/com/liferay/lms/learningactivity/TestLearningActivityType.java
@@ -282,6 +282,16 @@ public class TestLearningActivityType extends BaseLearningActivityType
 			showCorrectAnswer = SAXReaderUtil.createElement("showCorrectAnswer");
 			showCorrectAnswer.setText(Boolean.toString(ParamUtil.get(uploadRequest,"showCorrectAnswer",false)));		
 			rootElement.add(showCorrectAnswer);	
+			
+			Element showOnlyPreview = rootElement.element("showOnlyPreview");
+			if(showOnlyPreview!=null){
+				showOnlyPreview.detach();
+				rootElement.remove(showOnlyPreview);
+			}
+			showOnlyPreview = SAXReaderUtil.createElement("showOnlyPreview");
+			showOnlyPreview.setText(Boolean.toString(ParamUtil.get(uploadRequest, "showOnlyPreview", false)));
+			rootElement.add(showOnlyPreview);
+			
 			Element hideFeedback=rootElement.element("hideFeedback");
 			if(hideFeedback!=null)
 			{
@@ -301,7 +311,6 @@ public class TestLearningActivityType extends BaseLearningActivityType
 			showCorrectAnswerOnlyOnFinalTry.setText(Boolean.toString(ParamUtil.get(uploadRequest,"showCorrectAnswerOnlyOnFinalTry",false)));		
 			rootElement.add(showCorrectAnswerOnlyOnFinalTry);	
 			
-
 			Element improve=rootElement.element("improve");
 			if(improve!=null)
 			{
